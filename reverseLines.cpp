1b13ca4560ad5897ea6c5015e5e6b2564b4af58b
John Carvajal <35777566+JohnVernon96@users.noreply.github.com>
2018-01-30 23:40:59 -0600

    Update reverseLines.cpp

--- reverseLines.cpp
+++ reverseLines.cpp
@@ -7,10 +7,13 @@
 //		 read and reversed. 
 // Usage       : Pass the file name in with the command line argument
 
+
+//includes
 #include <iostream>
 #include <fstream>
 #include <string>
 
+//function prototypes
 void reverseString(std::string &str);
 
 int main(int argc,char** argv)
@@ -25,31 +28,50 @@ int main(int argc,char** argv)
 	else
 	{
 		//try to open the file
+		//create string named fileName
 		std::string fileName;
+		
+		//store command line argument in fileName
 		fileName = argv[1];
+		
+		//create ifstream object and pass it file name
 		std::ifstream inFile(fileName.c_str());
+		
+		//error message if file name does not exist
 		if(!inFile)
 		{
 			std::cout << "Error: Unable to open file named " << fileName << std::endl;
 			return -1;
 		}
-
+		
+                //print file name
 		std::cout << "File name is: " << fileName << std::endl << std::endl;
 
 		//read in lines and perform operation
+		//make new string called str
 		std::string str;
+		
+		//loop through reading all lines of data in the file
 		while(getline(inFile, str))
 		{
 			std::cout << "original line is:\n" << str << std::endl;
+			
+			//method to reverse str;
 			reverseString(str);
+			
+			//print modified str
 			std::cout << "reversed line is:" << str << std::endl << std::endl;
 		}
+		
+		//close file
 		inFile.close();
+		
 		std::cout << "Program has ended.\n";
 	}
 	return 0;
 }
 
+//functions
 void reverseString(std::string &str)
 {
 	int n = str.length();
@@ -60,4 +82,3 @@ void reverseString(std::string &str)
 		std::swap(str[i], str[n-i-1]);
 	}
 }
-
